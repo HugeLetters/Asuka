2a03e6b4d106115f0ade4a1a173a59ce300b8b30
HugeLetters <ckuka71@gmail.com>
2022-12-10 20:11:50 +0300

    Clean-up

--- Asuka.js
+++ Asuka.js
@@ -9,9 +9,7 @@ const token = process.env.DISCORD_TOKEN
 
 const headers = { "Authorization": `Bot ${token}` }
 const protocol = { http: "https", websocket: "wss" }
-let url;
-//url = {http:`${protocol.http}://discord.com/api/v10/applications/${app}/guilds/${guild}`}
-url = { http: `${protocol.http}://discord.com/api/v10` }
+let url = { http: `${protocol.http}://discord.com/api/v10` }
 
 const response = await fetch(url.http + "/gateway/bot", {
     method: 'GET',
@@ -20,7 +18,7 @@ const response = await fetch(url.http + "/gateway/bot", {
 
 url.ws = response.url;
 let hb_interval;
-let lastSkylexmessage = 0;
+let lastSkylexmessage = process.env.SKYLEX_LAST_MSG_TIME;
 console.log(url.ws);
 let auth = false;
 let last_seq = null;
@@ -43,6 +41,8 @@ socket.onmessage = async function (event) {
         hb_interval = data.d.heartbeat_interval;
         setTimeout(() => (heartbeat(socket, last_seq)), hb_interval * Math.random() / 10)
     };
+    // hearbeat immediate
+    if (data.op == 1) heartbeat(socket, last_seq);
     //auth
     if (!auth & data.op == 11) {
         console.log("Trying to authenticate");
@@ -67,22 +67,58 @@ socket.onmessage = async function (event) {
         auth = true
     };
     // SKYLEX печатает
-    if (auth & lastSkylexmessage == 0
-        // & data.t == "TYPING_START" & data.d.member.user.id == "214013992052588545" & (Date().getTime() - lastSkylexmessage) > 10800000
+    if (auth &&
+        data.t == "TYPING_START" &&
+        data.d.member.user.id == "214013992052588545" &&
+        (new Date().getTime() - lastSkylexmessage) > eval(process.env.SKYLEX_DELAY)
     ) {
         lastSkylexmessage = new Date().getTime();
-
         const files = readdirSync('source/');
         const filename = randomChoiceArray(files);
         const filepath = "./source/" + filename;
         let file = "data:image/png;base64," + readFileSync(filepath, "base64");
         file = dataURItoBlob(file);
 
+        const l = randomInteger(0, 1);
+        const skylex = randomChoiceArray([
+            `${["С", "c"][l]}кайлекс`,
+            `${["S", "s"][l]}kylex`,
+            `${["Г", "г"][l]}ефт`,
+            `${["Г", "г"][l]}уфт`
+        ]);
+
         let body = new FormData();
-        body.append("content", `${"Эй, ".repeat(randomInteger(0,1))}${"кто-то" || data.d.member.user.username} печатает${"!".repeat(randomInteger(1, 5))}`);
+        body.append("payload_json",
+            JSON.stringify({
+                content: `${"Смотрите! ".repeat(randomInteger(0, 1))}${skylex} печатает${"!".repeat(randomInteger(1, 5))}`
+            }));
         body.append("files[0]", file, filename);
 
-        fetch(`${url.http}/channels/${"1048360803373432844" || data.d.channel_id}/messages`, {
+        fetch(`${url.http}/channels/${data.d?.channel_id || "1048360803373432845"}/messages`, {
+            method: 'POST',
+            headers: headers,
+            body: body
+        });
+    }
+    // INTRODUCTION
+    if (auth &&
+        data.t == "MESSAGE_CREATE" &&
+        data.d.content == "асука привет"
+    ) {
+        const files = readdirSync('source/');
+        const filename = randomChoiceArray(files);
+        const filepath = "./source/" + filename;
+        let file = "data:image/png;base64," + readFileSync(filepath, "base64");
+        file = dataURItoBlob(file);
+        let body = new FormData();
+        body.append("payload_json",
+            JSON.stringify({
+                content: `Ну привет${", " + data.d.author.username || ""}`,
+                message_reference: { message_id: data.d.id }
+            }));
+        body.append("files[0]", file, filename);
+
+        fetch(`${url.http}/channels/${data.d?.channel_id || "1048360803373432845"}/messages`, {
             method: 'POST',
             headers: headers,
             body: body
