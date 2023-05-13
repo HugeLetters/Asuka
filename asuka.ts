577322bcfc4524728c8c2b684de429948dd27992
Huge Letters <ckuka71@gmail.com>
2023-05-13 13:53:56 +0200

    test commit

--- asuka.ts
+++ asuka.ts
@@ -6,7 +6,7 @@ async function fetch(url: string) {
   }
   return "Your data :)";
 }
-
+// test
 const fetchData = (url: string) => {
   return new Promise(resolve => {
     const getDelay = (function () {
